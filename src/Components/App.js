70d10d89a452e73134e2263efa071f3bb8bfa1f6
barit <mmyers.com@Gmail.com>
2023-09-25 13:30:29 -0500

    built app framework

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import {BrowserRouter as Router, Route, Routes} from `react-router-dom`
+import Navbar from `./Navbar`;
+import Gallery from `./Gallery`;
+import About from `./About`;
+import Favorites from `./Favorites`;
+
+useEffect(() => {
+  fetch('http://localhost:3000/Artwork')
+  .then((res) => res.json())
+  .then((data) => setArtworks(data));
+}, []);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Gallery />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Favorites />} />
+      </Routes>
+    </Router>
+  )
+};
 
 export default App;
