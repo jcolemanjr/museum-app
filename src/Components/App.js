a39909638df94dd1ff5057c31992471f119386c5
Christopher Orth <christopherorth430@yahoo.com>
2023-09-25 17:39:34 -0400

    cleaned up code and handled some errors

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 import Gallery from './Gallery';
 import About from './About';
 import Favorites from './Favorites';
@@ -16,17 +16,19 @@ function App() {
     .then((res) => res.json())
     .then((data) => setArtworks(data));
   }, []);
+  console.log("Hi")
+
+  //FOR A HEADER LETS GO AHEAD AND JUST PLACE IT INSIDE OF EACH COMPONENT TO CREATE THE SAME HEADER FOR EACH
 
   return (
-    <div>
-      <Navbar />
+  
       <Routes>
         <Route path="/Gallery" element={<Gallery artworks={artworks} setArtworks={setArtworks} />} />
         <Route path="/About" element={<About />} />
         <Route path="/Favorites" element={<Favorites />} />
       </Routes>
-    </div>
-  )
+     
+   )
 };
 
 export default App;
