e3dfebfaffcc73a11f8c5aab5c64f42eb61d5693
barit <mmyers.com@Gmail.com>
2023-09-25 20:43:32 -0500

    Fixed up issues with Artwork.js

--- src/Components/Artwork.js
+++ src/Components/Artwork.js
@@ -1,35 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Artwork( { artist, title, image, date, medium, description } ) {
-
-    const [isartist, setisArtist] = useState(true)
-
-    function artistCheck() {
-        if (artist == "") {
-            return "Unkown Artist"
-        } else {
-            return artist
-        }
+function Artwork({ artist, title, image, date, medium, description }) {
+  function artistCheck() {
+    if (artist === "") {
+      return "Unknown Artist";
+    } else {
+      return artist;
     }
+  }
 
-    function descCheck() {
-        if (description == "" || " ") {
-            return `${title} ${artistCheck} ${date}`
-        } else {
-        return description
+  function descCheck() {
+    if (description === "" || description === " ") {
+      return `${title} ${artistCheck()} ${date}`;
+    } else {
+      return description;
     }
+  }
 
-    return (
-        <div>
-            <h1>{title}</h1>
-            <img onClick={console.log("Works")} src={image} alt={`This is a piece called ${description} by ${artist}`} title={title} />
-            <h2>{artistCheck}</h2>
-            <p>{date}</p>
-            <p>{medium}</p>
-            <p>{descCheck}</p>
-        </div>
-    )}
+  return (
+    <div>
+      <h1>{title}</h1>
+      <img
+        onClick={() => console.log("Works")}
+        src={image}
+        alt={`This is a piece called ${description} by ${artist}`}
+        title={title}
+      />
+      <h2>{artistCheck()}</h2>
+      <p>{date}</p>
+      <p>{medium}</p>
+      <p>{descCheck()}</p>
+    </div>
+  );
 }
-
 
 export default Artwork;
